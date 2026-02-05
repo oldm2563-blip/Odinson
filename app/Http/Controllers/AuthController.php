277c54d6b6c70b19a7b02ecbee845917949177a7
Oldman2563 <oldm2563@gmail.com>
2026-02-05 10:04:47 +0100

    make the authcontroller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login_form(){
+        if(!auth()->check()){
+            return view('auth.login');
+        }
+        else{
+            return redirect('/');
+        }
+    }
+    public function register_form(){
+        if(!auth()->check()){
+            return view ('auth.register');
+        }
+        else{
+            return redirect('/');
+        }
+    }
+}
