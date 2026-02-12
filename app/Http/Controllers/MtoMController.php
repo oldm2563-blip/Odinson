1a1840ad91d64b0f8c6fc8cee89b871658e3ee34
Oldman2563 <oldm2563@gmail.com>
2026-02-12 10:33:31 +0100

    add the favorites method

--- app/Http/Controllers/MtoMController.php
+++ app/Http/Controllers/MtoMController.php
@@ -2,16 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Log;
 use App\Models\Link;
 use Illuminate\Http\Request;
 
 class MtoMController extends Controller
 {
-    public function give_tag(Link $link, Request $request){
+    public function give_tag(Link $link, Request $request)
+    {
         $incomingFields = $request->validate([
             'tag_id' => 'required'
         ]);
-        $link->tag()->attach($incomingFields['tag_id']);
+        $link->tag()->syncWithoutDetaching($incomingFields['tag_id']);
+        Log::record('Assigned Tag to', 'link', $link->id);
         return redirect()->back()->with('success', 'new Tag has been made');
+    }
+
+    public function favor(Link $link)
+    {
+        $link->favorits()->syncWithoutDetaching(auth()->id());
+        return redirect()->back()->with('success', 'Link Has Been Set As Favorite');
     }
 }
